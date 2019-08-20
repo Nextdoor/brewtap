7979128d00b2a33239d15644b289bdbc41258e5b
Slava Markeyev <slava@nextdoor.com>
2019-08-20 17:56:58 +0000

    first commit

---
+++ Formula/mosh.rb
@@ -0,0 +1,33 @@
+class Mosh < Formula
+  desc "Remote terminal application"
+  homepage "https://mosh.org"
+
+  head "https://github.com/mobile-shell/mosh.git", :revision => "b1da7005358b4a550e31faa646cf1cf116700412"
+  head do
+    url "https://github.com/mobile-shell/mosh.git", :shallow => false
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "tmux" => :build
+  depends_on "protobuf"
+
+  def install
+    ENV.cxx11
+
+    # teach mosh to locate mosh-client without referring
+    # PATH to support launching outside shell e.g. via launcher
+    inreplace "scripts/mosh.pl", "'mosh-client", "\'#{bin}/mosh-client"
+
+    system "./autogen.sh" if build.head?
+    system "./configure", "--prefix=#{prefix}", "--enable-completion"
+    system "make", "check"
+    system "make", "install"
+  end
+
+  test do
+    system bin/"mosh-client", "-c"
+  end
+end
