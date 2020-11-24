d3e898e59be1189e33335e4ccf7777e0259efa1c
Slava Markeyev <slava@nextdoor.com>
2020-11-24 22:43:45 +0000

    Create carthage@0.0.1.rb
    Use forked Carthage version

---
+++ Formula/carthage@0.0.1.rb
@@ -0,0 +1,27 @@
+class CarthageAT001 < Formula
+  desc "Decentralized dependency manager for Cocoa"
+  homepage "https://github.com/Carthage/Carthage"
+  license "MIT"
+  url "https://github.com/Nextdoor/Carthage/archive/0.36.0-0.0.1.zip"
+  head "https://github.com/Nextdoor/Carthage.git"
+
+  bottle do
+    root_url "https://github.com/Nextdoor/Carthage/releases/download/0.36.0-0.0.1"
+    cellar :any
+    sha256 "666938dd5508ec5868a3f54e927c8fa6a78a84d84d8885bed805548f6a09701b" => :catalina
+  end
+
+  depends_on xcode: ["10.0", :build]
+
+  def install
+    system "make", "prefix_install", "PREFIX=#{prefix}"
+    bash_completion.install "Source/Scripts/carthage-bash-completion" => "carthage"
+    zsh_completion.install "Source/Scripts/carthage-zsh-completion" => "_carthage"
+    fish_completion.install "Source/Scripts/carthage-fish-completion" => "carthage.fish"
+  end
+
+  test do
+    (testpath/"Cartfile").write 'github "jspahrsummers/xcconfigs"'
+    system bin/"carthage", "update"
+  end
+end
